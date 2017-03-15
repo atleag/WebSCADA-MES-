56a6cf23d3470281ebdabb512ac6d59b324a2d06
Moskoskos <atleag@outlook.com>
2017-03-15 09:52:36 +0100

    Fixedd foregin key

--- WatermentWebSCADA/WatermentWebSCADA/Models/ADOEDM.Designer.cs
+++ WatermentWebSCADA/WatermentWebSCADA/Models/ADOEDM.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Jon Arne\Desktop\WebSCADA\WebSCADA\WatermentWebSCADA\WatermentWebSCADA\Models\ADOEDM.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\W840S\Source\Repos\WebSCADA\WatermentWebSCADA\WatermentWebSCADA\Models\ADOEDM.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
