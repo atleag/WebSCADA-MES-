bea95b31624ff4a9480ec6e9f451d1ebd7b5cda0
Moskoskos <atleag@outlook.com>
2017-03-20 13:08:05 +0100

    123

---
+++ WatermentWebSCADA/WatermentWebSCADA/Controllers/MainController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WatermentWebSCADA.Models;
+using WatermentWebSCADA.ViewModels;
+
+namespace WatermentWebSCADA.Controllers
+{
+    public class mainController : Controller
+    {
+        private watermentdbEntities db = new watermentdbEntities();
+        // GET: Main
+        public ActionResult Index(int? id)
+        {
+
+            var data = new mainViewModel {
+                IP = db.facilities.Select(x => x.IP).FirstOrDefault(),
+                Name = db.facilities.Select(x => x.Name).FirstOrDefault(),
+                Address = db.country.Select(x => x.CountryName).FirstOrDefault()
+            };
+            
+            return View(data);
+        }
+    }
+}
---
+++ WatermentWebSCADA/WatermentWebSCADA/ViewModels/mainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WatermentWebSCADA.Models;
+
+namespace WatermentWebSCADA.ViewModels
+{
+    public class mainViewModel
+    {
+
+        public facilities facilitiess { get; set; }
+        
+        
+        //facilities
+        public string Name { get; set; }
+        public string IP { get; set; }
+        public string Domain { get; set; }
+        //locations model
+        public string Address { get; set; }
+        public int Postcode { get; set; }
+        public string County { get; set; }
+
+
+    }
+}
