c6db80b9632b9b63649ca677d9785a50fbe02bff
sdfgh <sdfgh@DESKTOP-Q6QPJ3P>
2017-03-21 13:44:02 +0100

    fc

--- WatermentWebSCADA/WatermentWebSCADA/ViewModels/mainViewModel.cs
+++ WatermentWebSCADA/WatermentWebSCADA/ViewModels/mainViewModel.cs
@@ -7,5 +7,16 @@ namespace WatermentWebSCADA.ViewModels
 {
     public class MainViewModel
     {
+        public facilities facilitiess { get; set; }
+
+
+        //facilities
+        public string Name { get; set; }
+        public string IP { get; set; }
+        public string Domain { get; set; }
+        //locations model
+        public string Address { get; set; }
+        public int Postcode { get; set; }
+        public string County { get; set; }
     }
 }
