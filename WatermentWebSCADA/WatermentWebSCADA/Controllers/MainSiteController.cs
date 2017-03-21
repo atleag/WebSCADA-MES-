626fb3d493e44813a56a074e6b08fa43fffc9897
sdfgh <sdfgh@DESKTOP-Q6QPJ3P>
2017-03-21 13:42:13 +0100

    added controller

---
+++ WatermentWebSCADA/WatermentWebSCADA/Controllers/MainSiteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WatermentWebSCADA.Controllers
+{
+    public class mainController : Controller
+    {
+        private watermentdbEntities db = new watermentdbEntities();
+        // GET: Main
+        public ActionResult Index(int? id)
+        {
+
+            var data = new mainViewModel
+            {
+                IP = db.facilities.Select(x => x.IP).FirstOrDefault(),
+                Name = db.facilities.Select(x => x.Name).FirstOrDefault(),
+                Address = db.country.Select(x => x.CountryName).FirstOrDefault(),
+            };
+
+            return View(data);
+        }
+    }
