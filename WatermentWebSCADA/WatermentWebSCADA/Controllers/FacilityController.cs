c289d0f76ed1920bf559d04aa3e678b56c6eb5b7
Kimmeh <kimomsland@hotmail.com>
2017-03-29 14:23:23 +0200

    kimkok

--- WatermentWebSCADA/WatermentWebSCADA/Controllers/FacilityController.cs
+++ WatermentWebSCADA/WatermentWebSCADA/Controllers/FacilityController.cs
@@ -19,27 +19,28 @@ namespace WatermentWebSCADA.Controllers
     public class FacilityController : Controller
     {
         Models.watermentdbEntities db = new Models.watermentdbEntities();
-        public string landnavnKim;
+       
         int LandId1;
+        int LokasjonsID;
+        int BrukerID;
         // GET: Facility
         public ActionResult FacilityDetails(int? id)
         {
-            var landid = db.facilities.Where(b => b.Id == id);
+           
 
             foreach (var item in db.facilities.Where(c=>c.Id==id))
             {
-
                 LandId1 = item.locations_countries_Id;
-
-
-
+                LokasjonsID = item.locations_Id;
+             
             }
+
 
             using (var db = new Models.watermentdbEntities())
             {
                 var model = new MainViewModel
                 {
-                    Alarmer = db.alarms.ToList(),
+                    Alarmer = db.alarms.Where(x=>x.equipments_facilities_Id==id).Where(o=>o.Status=="Active").ToList(),
                     Anlegg = db.facilities.Where(c => c.Id == id).ToList(),
 
                     //Anlegg2=db.facilities.Where(x=>x.locations_countries_Id=landid),
@@ -49,10 +50,10 @@ namespace WatermentWebSCADA.Controllers
                     Land = db.countries.Where(x => x.Id == LandId1).ToList(), /*Se her mer 167 som lokal variabel fra koden før*/
                     //Land = db.countries.ToList(),
                     Utstyr = db.equipments.ToList(),
-                    Lokasjoner = db.locations.ToList(),
+                    Lokasjoner = db.locations.Where(x=>x.Id==LokasjonsID).ToList(),
                     Vedlikehold = db.maintenance.ToList(),
                     Roller = db.roles.ToList(),
-                    Brukere = db.users.ToList(),
+                    Brukere = db.users.Where(x=>x.locations_Id==LokasjonsID).ToList(),
                     Sesjoner = db.sessions.ToList(),
 
                 };
