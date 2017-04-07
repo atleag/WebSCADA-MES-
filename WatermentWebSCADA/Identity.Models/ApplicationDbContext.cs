696aa563d28b80b94834755dca5f5d57036d0d32
Atle-André Gundersen <atleag@outlook.com>
2017-04-07 09:20:34 +0200

    Added IdentityFrameWork

---
+++ WatermentWebSCADA/Identity.Models/ApplicationDbContext.cs
@@ -0,0 +1,59 @@
+﻿namespace codingfreaks.samples.Identity.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
+
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    /// <summary>
+    /// A basic implementation for an application database context compatible with ASP.NET Identity 2 using
+    /// <see cref="long"/> as the key-column-type for all entities.
+    /// </summary>
+    /// <remarks>
+    /// This type depends on some other types out of this assembly.
+    /// </remarks>
+    public class ApplicationDbContext : IdentityDbContext<MyUser, MyRole, long, MyLogin, MyUserRole, MyClaim>
+    {
+        #region constructors and destructors
+
+        public ApplicationDbContext()
+            : base("IdentityConnection")
+        {
+        }
+
+        #endregion
+
+        #region methods
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Map Entities to their tables.
+            modelBuilder.Entity<MyUser>().ToTable("User");
+            modelBuilder.Entity<MyRole>().ToTable("Role");
+            modelBuilder.Entity<MyClaim>().ToTable("UserClaim");
+            modelBuilder.Entity<MyLogin>().ToTable("UserLogin");
+            modelBuilder.Entity<MyUserRole>().ToTable("UserRole");
+            // Set AutoIncrement-Properties
+            modelBuilder.Entity<MyUser>().Property(r => r.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<MyClaim>().Property(r => r.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<MyRole>().Property(r => r.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            // Override some column mappings that do not match our default
+            modelBuilder.Entity<MyUser>().Property(r => r.UserName).HasColumnName("UserName");
+            modelBuilder.Entity<MyUser>().Property(r => r.PasswordHash).HasColumnName("PasswordHash");
+            modelBuilder.Entity<MyUser>().Property(r => r.FirstName).HasColumnName("FirstName");
+            modelBuilder.Entity<MyUser>().Property(r => r.LastName).HasColumnName("LastName");
+
+
+
+
+        }
+
+        #endregion
+    }
+}
---
+++ WatermentWebSCADA/Identity.Models/EmailService.cs
@@ -0,0 +1,19 @@
+﻿namespace codingfreaks.samples.Identity.Models
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNet.Identity;
+
+    public class EmailService : IIdentityMessageService
+    {
+        #region methods
+
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+
+        #endregion
+    }
+}
