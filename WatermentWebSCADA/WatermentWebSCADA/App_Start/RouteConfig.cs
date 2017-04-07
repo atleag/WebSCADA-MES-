5d40b5e332f3d892bc0e5aa0d8aac52d588c3767
Atle-André Gundersen <atleag@outlook.com>
2017-04-07 09:23:01 +0200

    Changed start controller from Login to Account

--- WatermentWebSCADA/WatermentWebSCADA/App_Start/RouteConfig.cs
+++ WatermentWebSCADA/WatermentWebSCADA/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WatermentWebSCADA
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
