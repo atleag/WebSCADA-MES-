b48489c400d2f3c90dc0a345470c3bfd36996be9
Kimmeh <kimomsland@hotmail.com>
2017-04-16 15:40:24 +0200

    Export til Excel implementert

---
+++ WatermentWebSCADA/WatermentWebSCADA/Controllers/ExportController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using WatermentWebSCADA.Models;
+using System.IO;
+using System.Web.UI;
+
+using WatermentWebSCADA.ViewModels;
+
+namespace WatermentWebSCADA.Controllers
+{
+    public class ExportController : Controller
+    {
+        Models.watermentdbEntities db = new Models.watermentdbEntities();
+
+
+        
+
+        public ActionResult Index(int? id)
+        {
+
+            using (var db = new Models.watermentdbEntities())
+            {
+                var model = new MainViewModel
+                {
+
+                    AlarmList = db.alarms.Where(x => x.equipments_facilities_Id == id).Where(o => o.Status == "Active").ToList(),
+              };
+                return View(model);
+            }
+        }
+
+
+        public ActionResult ExportClientsListToExcel()
+        {
+
+            using (var db = new Models.watermentdbEntities())
+            {
+                var model = new MainViewModel
+                {
+
+                    AlarmList = db.alarms.Where(o => o.Status == "Active").ToList(),
+
+
+
+
+                };
+                var grid = new System.Web.UI.WebControls.GridView();
+
+                grid.DataSource = 
+                                  from d in db.measurements.ToList()
+                                  select new
+                                  {
+                                      ID = d.Id,
+                                      ProcessValue = d.ProcessValue,
+                                      Recorded = d.Recorded,
+                                      Equipment = d.equipments_Id,
+                                      EqupmentName = d.equipments.Tag
+                                      
+
+                                  };
+
+                grid.DataBind();
+
+                Response.ClearContent();
+                Response.AddHeader("content-disposition", "attachment; filename=MeasurementList.xls");
+                Response.ContentType = "application/excel";
+                StringWriter sw = new StringWriter();
+                HtmlTextWriter htw = new HtmlTextWriter(sw);
+
+                grid.RenderControl(htw);
+
+                Response.Write(sw.ToString());
+
+                Response.End();
+
+                return null;
+            }
+        }
+
+        
+    }
+}
