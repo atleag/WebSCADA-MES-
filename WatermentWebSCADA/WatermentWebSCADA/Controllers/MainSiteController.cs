1586db25e214d66651998aacbdba18e3a4aa1dda
sdfgh <sdfgh@DESKTOP-Q6QPJ3P>
2017-04-17 16:15:57 +0200

    fixed statusupdat whit ping an updat in db

--- WatermentWebSCADA/WatermentWebSCADA/Controllers/MainSiteController.cs
+++ WatermentWebSCADA/WatermentWebSCADA/Controllers/MainSiteController.cs
@@ -13,6 +13,7 @@ using MySql.Data.Entity;
 using System.Data.Common;
 using System.Web.Helpers;
 using WatermentWebSCADA.Models;
+using System.Net.NetworkInformation;
 
 namespace WatermentWebSCADA.Controllers
 {
@@ -20,37 +21,79 @@ namespace WatermentWebSCADA.Controllers
     {
         Models.watermentdbEntities db = new Models.watermentdbEntities();
 
-        
-        
+        public bool PingHost(string ip)
+        {
+            string nameOrAddress = ip;
+            bool pingable = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+             
+            }
+            return pingable;
+        }
+
+        string IpFacility;
+
         // GET: Main
         public ActionResult Index(int? id, string sortOrder)
-        {
-            using (var db1 = new Models.watermentdbEntities())
+
+       
 
             {
-                var model = new MainViewModel
+
+
+            foreach (var item in db.facilities)
+            {
+                IpFacility = item.IP;
+
+               if ( PingHost(IpFacility) == true)
                 {
+                    item.FacilityStatus_Id = 2;
 
-                    Countries = db.countries.Where(c => c.continents.Id == id).ToList(),
-                    AlarmList = db.alarms.Where(o => o.Status == "Active").ToList(),
-                    Equipment = db.equipments.Include(c => c.alarms).Include(c => c.facilities).ToList(),
-                    Lokasjoner = db.locations.ToList(),
-                    Vedlikehold = db.maintenance.OrderBy(x => x.lastMaintenance).Take(10).ToList(),
-                    Facilites = db.facilities.ToList(),
+                }
+               else
+                {
+                    item.FacilityStatus_Id = 1;
+                }
 
-                    antallFacilities = db.facilities.Count(),
-                    antallOnline = db.facilities.Where(x=> x.FacilityStatus_Id == 1).Count(),
-                    antallOffline = db.facilities.Where(x => x.FacilityStatus_Id == 2).Count(),
-                };
+           
+              
+            }
+            db.SaveChanges();
 
-                   
+            using (var db1 = new Models.watermentdbEntities())
+
+                {
+                    var model = new MainViewModel
+                    {
+
+                        Countries = db.countries.Where(c => c.continents.Id == id).ToList(),
+                        AlarmList = db.alarms.Where(o => o.Status == "Active").ToList(),
+                        Equipment = db.equipments.Include(c => c.alarms).Include(c => c.facilities).ToList(),
+                        Lokasjoner = db.locations.ToList(),
+                        Vedlikehold = db.maintenance.OrderBy(x => x.lastMaintenance).Take(10).ToList(),
+                        Facilites = db.facilities.ToList(),
+
+                        antallFacilities = db.facilities.Count(),
+                        antallOnline = db.facilities.Where(x => x.FacilityStatus_Id == 2).Count(),
+                        antallOffline = db.facilities.Where(x => x.FacilityStatus_Id == 1).Count(),
+                    };
 
 
-                return View(model);
 
-            }   
-           }      
-      
-        } 
+
+                    return View(model);
+
+                }
+            }
+
+        }
 
     }
+
---
+++ WatermentWebSCADA/WatermentWebSCADA/Controllers/facilities3Controller.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WatermentWebSCADA.Models;
+
+namespace WatermentWebSCADA.Controllers
+{
+    public class facilities3Controller : Controller
+    {
+        private watermentdbEntities db = new watermentdbEntities();
+
+        // GET: facilities3
+        public async Task<ActionResult> Index()
+        {
+            var facilities = db.facilities.Include(f => f.FacilityStatus).Include(f => f.locations);
+            return View(await facilities.ToListAsync());
+        }
+
+        // GET: facilities3/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            facilities facilities = await db.facilities.FindAsync(id);
+            if (facilities == null)
+            {
+                return HttpNotFound();
+            }
+            return View(facilities);
+        }
+
+        // GET: facilities3/Create
+        public ActionResult Create()
+        {
+            ViewBag.FacilityStatus_Id = new SelectList(db.FacilityStatus, "Id", "Name");
+            ViewBag.locations_Id = new SelectList(db.locations, "Id", "StreetAddress");
+            return View();
+        }
+
+        // POST: facilities3/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,IP,Domain,locations_Id,locations_countries_Id,locations_countries_continents_Id,FacilityStatus_Id")] facilities facilities)
+        {
+            if (ModelState.IsValid)
+            {
+                db.facilities.Add(facilities);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FacilityStatus_Id = new SelectList(db.FacilityStatus, "Id", "Name", facilities.FacilityStatus_Id);
+            ViewBag.locations_Id = new SelectList(db.locations, "Id", "StreetAddress", facilities.locations_Id);
+            return View(facilities);
+        }
+
+        // GET: facilities3/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            facilities facilities = await db.facilities.FindAsync(id);
+            if (facilities == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FacilityStatus_Id = new SelectList(db.FacilityStatus, "Id", "Name", facilities.FacilityStatus_Id);
+            ViewBag.locations_Id = new SelectList(db.locations, "Id", "StreetAddress", facilities.locations_Id);
+            return View(facilities);
+        }
+
+        // POST: facilities3/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,IP,Domain,locations_Id,locations_countries_Id,locations_countries_continents_Id,FacilityStatus_Id")] facilities facilities)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(facilities).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FacilityStatus_Id = new SelectList(db.FacilityStatus, "Id", "Name", facilities.FacilityStatus_Id);
+            ViewBag.locations_Id = new SelectList(db.locations, "Id", "StreetAddress", facilities.locations_Id);
+            return View(facilities);
+        }
+
+        // GET: facilities3/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            facilities facilities = await db.facilities.FindAsync(id);
+            if (facilities == null)
+            {
+                return HttpNotFound();
+            }
+            return View(facilities);
+        }
+
+        // POST: facilities3/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            facilities facilities = await db.facilities.FindAsync(id);
+            db.facilities.Remove(facilities);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
