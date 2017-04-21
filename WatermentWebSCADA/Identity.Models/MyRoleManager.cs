982177c952a58647e28c1acc90d2c2d96833570f
Atle-André Gundersen <atleag@outlook.com>
2017-04-21 19:55:03 +0200

    RoleModel and Manager updated

--- WatermentWebSCADA/Identity.Models/MyRoleManager.cs
+++ WatermentWebSCADA/Identity.Models/MyRoleManager.cs
@@ -25,16 +25,3 @@ namespace codingfreaks.samples.Identity.Models
     }
 }
 
-namespace codingfreaks.samples.Identity.Models
-{
-    using Microsoft.AspNet.Identity.EntityFramework;
-
-    public class MyRole : IdentityRole<long, MyUserRole>
-    {
-        public MyRole() : base() { }
-
-        //public MyRole(string name) : base(name) { }
-
-        public string Description { get; set; }
-    }
-}
