a2db2fe898798edc5c2a4742a0c9015cc76d316a
Jon Arne <karlsener@gmail.com>
2017-04-21 15:27:07 +0200

    Endret overview siden

--- WatermentWebSCADA/WatermentWebSCADA/Controllers/MainSiteController.cs
+++ WatermentWebSCADA/WatermentWebSCADA/Controllers/MainSiteController.cs
@@ -97,59 +97,6 @@ namespace WatermentWebSCADA.Controllers
 
                 }
             }
-        public ActionResult Index2(int? id, string sortOrder)
-
-
-
-        {
-
-
-            foreach (var item in db.facilities)
-            {
-                IpFacility = item.IP;
-
-                if (PingHost(IpFacility) == true)
-                {
-                    item.FacilityStatus_Id = 2;
-
-                }
-                else
-                {
-                    item.FacilityStatus_Id = 1;
-                }
-
-
-
-            }
-            db.SaveChanges();
-
-            using (var db1 = new Models.watermentdbEntities())
-
-            {
-                var model = new MainViewModel
-                {
-
-                    Countries = db.countries.Where(c => c.continents.Id == id).ToList(),
-                    AlarmList = db.alarms.Where(o => o.Status == "Active").ToList(),
-                    Equipment = db.equipments.Include(c => c.alarms).Include(c => c.facilities).ToList(),
-                    Lokasjoner = db.locations.ToList(),
-                    Vedlikehold = db.maintenance.OrderBy(x => x.LastMaintenance).Take(10).ToList(),
-                    Facilites = db.facilities.ToList(),
-
-                    antallFacilities = db.facilities.Count(),
-                    antallOnline = db.facilities.Where(x => x.FacilityStatus_Id == 2).Count(),
-                    antallOffline = db.facilities.Where(x => x.FacilityStatus_Id == 1).Count(),
-                    noAlarms = db.alarms.Where(x => x.Status == "Active").Count(),
-                };
-
-
-
-
-                return View(model);
-
-            }
-        }
-
         public ActionResult About()
         {
             return View();           
