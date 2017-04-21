8d7e9d89f5044661a6ded9453ce58bc82e3a3b6c
Atle-André Gundersen <atleag@outlook.com>
2017-04-21 19:47:19 +0200

    RoleViewModel

---
+++ WatermentWebSCADA/WatermentWebSCADA/ViewModels/RoleViewModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WatermentWebSCADA.ViewModels
+{
+
+    public class RoleVM
+    {
+        public string Name { get; }
+        public string Description { get; }
+
+
+
+    }
+}
