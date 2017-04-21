6f5b2c6dcf24eb82b8513f01ace828f4ec4710c5
Atle-André Gundersen <atleag@outlook.com>
2017-04-21 19:51:38 +0200

    RoleManagerUpdate

---
+++ WatermentWebSCADA/Identity.Models/MyRoleManager.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace codingfreaks.samples.Identity.Models
+{
+    public class MyRoleManager : RoleManager<MyRole, long>
+    {
+        public MyRoleManager(IRoleStore<MyRole, long> roleStore)
+            : base(roleStore)
+        {
+        }
+
+        public static MyRoleManager Create(IdentityFactoryOptions<MyRoleManager> options, IOwinContext context)
+        {
+            return new MyRoleManager(new RoleStore<MyRole, long, MyUserRole>(context.Get<ApplicationDbContext>()));
+        }
+    }
+}
+
+namespace codingfreaks.samples.Identity.Models
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class MyRole : IdentityRole<long, MyUserRole>
+    {
+        public MyRole() : base() { }
+
+        //public MyRole(string name) : base(name) { }
+
+        public string Description { get; set; }
+    }
+}
