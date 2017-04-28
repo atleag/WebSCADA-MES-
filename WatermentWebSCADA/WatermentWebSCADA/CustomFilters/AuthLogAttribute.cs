b29dda7d0db31f9642c0376b4583671eb5daf102
Atle-André Gundersen <atleag@outlook.com>
2017-04-28 22:02:04 +0200

    removed a bug where the escape characters would display as text and updated the allignment of text on the error page

--- WatermentWebSCADA/WatermentWebSCADA/CustomFilters/AuthLogAttribute.cs
+++ WatermentWebSCADA/WatermentWebSCADA/CustomFilters/AuthLogAttribute.cs
@@ -47,7 +47,7 @@ namespace WatermentWebSCADA.CustomFilters
                 vr.ViewName = View;
 
                 ViewDataDictionary dict = new ViewDataDictionary();
-                dict.Add("Message", "You do not have sufficient privilages to view this page. /r/n If you are in need of accessing this page please contact the system administratior");
+                dict.Add("Message", "You do not have sufficient privilages to view this page. If you are in need of accessing this page please contact the system administratior");
 
                 vr.ViewData = dict;
 
