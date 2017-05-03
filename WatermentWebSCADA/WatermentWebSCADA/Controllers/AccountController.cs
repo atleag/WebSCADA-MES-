65bad9c33a62ee015f4ff9abc210709d054cd209
Atle-André Gundersen <atleag@outlook.com>
2017-05-03 18:59:43 +0200

    Restoring error hadnling if registermethod fails

--- WatermentWebSCADA/WatermentWebSCADA/Controllers/AccountController.cs
+++ WatermentWebSCADA/WatermentWebSCADA/Controllers/AccountController.cs
@@ -472,7 +472,7 @@ namespace WatermentWebSCADA.Controllers
 
                     return RedirectToAction("Index", "UserManagement");
                 }
-                return RedirectToAction("Register");
+                //return RedirectToAction("Register");
                 AddErrors(result);
                 
             }
