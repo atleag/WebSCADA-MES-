dd082b67003fd69cd87eca2a24f0ab491df7ca96
Atle-André Gundersen <atleag@outlook.com>
2017-05-05 11:11:12 +0200

    Documentation and credit of the Identity.Models

--- WatermentWebSCADA/Identity.Models/MyRoleManager.cs
+++ WatermentWebSCADA/Identity.Models/MyRoleManager.cs
@@ -1,4 +1,10 @@
-﻿
+﻿// Purpose: Enable ASP.NET Identity to support T4/DB-First and long / INT(11) as Primary Key.
+// All credits for this custom project / framework class goes to codingfreaks.de 
+// URL 1: http://www.codingfreaks.de/2014/01/11/microsoft-aspnet-identity-in-bestender-anwendung-einsetzen/
+// URL 2: http://www.codingfreaks.de/2014/06/16/microsoft-aspnet-identity-in-bestehender-anwendung-einsetzen-teil-2/
+// - Moskoskos
+
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +17,10 @@ using Microsoft.Owin;
 
 namespace codingfreaks.samples.Identity.Models
 {
+    /// <summary>
+    /// Extension of codefreaks custom framework for Identity to handle the creation of roles.
+    /// Moskoskos
+    /// </summary>
     public class MyRoleManager : RoleManager<MyRole, long>
     {
         public MyRoleManager(IRoleStore<MyRole, long> roleStore)
